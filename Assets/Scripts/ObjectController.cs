c90a3b52307d88f155326eb37f20776ddbf46293
harrison.pedro <hpedro1195@gmail.com>
2016-12-10 05:16:52 -0200

    Fixing conflicts

--- Assets/Scripts/ObjectController.cs
+++ Assets/Scripts/ObjectController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ObjectController : MonoBehaviour {
 
+	public Vector3 direction;
 	// Use this for initialization
 	void Start () {
-		
+		direction.Normalize ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter2D(Collision2D collisionObject){
+		Vector2 normal = collisionObject.contacts [0].normal;
+		direction = Vector2.
 	}
 }
